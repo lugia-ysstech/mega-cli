684ffa9a9174cdd7b2e994e0d58d0bfc8cab7bf6
hanjingbo <jingboup@gmail.com>
2019-01-25 10:36:58 +0800

    优化测试用例

--- packages/mega/test/singleBuild.test.js
+++ packages/mega/test/singleBuild.test.js
@@ -1,28 +1,29 @@
 import { join, parse } from 'path';
 import { readdirSync } from 'fs';
-import { fork } from 'child_process';
+import { exec } from 'child_process';
 import assertBuildResult from '../../mega-webpack/test/assertBuildResult';
 
 function testSingleBuild(cwd, done) {
   const { name } = parse(cwd);
-  const f = fork(join(__dirname, '../bin/mega.js'), ['build', `${name}.js`]);
 
-  f.on('close', code => {
-    if (code === 0) {
+  exec(
+    `node ${join(__dirname, '../bin/mega.js')} build ${name}.js`,
+    {
+      cwd,
+    },
+    err => {
+      if (err) {
+        throw new Error(err);
+      }
+
       try {
         assertBuildResult(cwd);
         done();
       } catch (e) {
         done(e);
       }
-    } else {
-      throw new Error(code);
-    }
-  });
-
-  f.on('error', err => {
-    throw new Error(err);
-  });
+    },
+  );
 }
 
 describe('singleBuild', () => {
@@ -35,7 +36,6 @@ describe('singleBuild', () => {
       const fn = dir.indexOf('-only') > -1 ? test.only : test;
       fn(dir, done => {
         const cwd = join(buildPath, dir);
-        process.chdir(cwd);
         testSingleBuild(cwd, done);
       });
     });
