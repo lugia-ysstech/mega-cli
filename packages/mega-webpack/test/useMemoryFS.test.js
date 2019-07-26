3c6ba2385e2b41945f4db571f4156b95c5c55bcd
hanjingbo <jingboup@gmail.com>
2019-07-26 18:21:24 +0800

    修改 useMemoryFS 测试用例
    (cherry picked from commit 638bbcddd9741b5c1c358afdf5b88d47c249c5bf)

--- packages/mega-webpack/test/useMemoryFS.test.js
+++ packages/mega-webpack/test/useMemoryFS.test.js
@@ -2,7 +2,7 @@
  * Created Date: Monday, January 28th 2019, 5:45:53 pm
  * Author: hanjingbo@ysstech.com | jingboup@gmail.com
  * -----
- * Last Modified: Thursday, January 31st 2019, 4:06:16 pm
+ * Last Modified: Friday, July 26th 2019, 6:21:15 pm
  * Modified By: hanjingbo <hanjingbo@ysstech.com | jingboup@gmail.com>
  * -----
  * Copyright (c) 2019-present, #Lugia#.
@@ -17,6 +17,7 @@ process.env.NODE_ENV = 'production';
 process.env.COMPRESS = 'none';
 process.env.ESLINT = 'none';
 process.env.TSLINT = 'none';
+// eslint-disable-next-line
 process.env.__FROM_TEST = true;
 
 describe('build', () => {
@@ -24,16 +25,18 @@ describe('build', () => {
     build({
       webpackConfig: getConfig({
         cwd: join(__dirname, './fixtures/normal'),
-        entry: join(__dirname, './fixtures/normal/index.js'),
+        entry: join(__dirname, './fixtures/normal/index.js')
       }),
       useMemoryFS: true,
       success({ assets }) {
-        expect(assets[0]).toMatchSnapshot({ path: expect.any(String) });
+        const js = assets[0];
+        js.content = js.content.toString();
+        expect(js).toMatchSnapshot({ path: expect.any(String) });
         done();
       },
       fail(err) {
         throw new Error(err);
-      },
+      }
     });
   });
 });
